c7fd07e32f0390f1ff187494fc46d10c9162bacc
Ada Yang <adayang@Chen-Yangs-MacBook-Pro.local>
2025-04-02 02:18:02 +0800

    first commit

---
+++ src/main/java/edu/sbs/cs/WebProject2025SpringApplication.java
@@ -0,0 +1,13 @@
+package edu.sbs.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProject2025SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProject2025SpringApplication.class, args);
+    }
+
+}
